4bd0dbad4f5d9d36d994efec5598e1e61988b6e5
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-11-01 13:31:37 -0500

    Se crea la estructura de un proyecto Eclipse, listo para empezar a trabajar.

---
+++ src/co/edu/unal/isi/taxiagent2/gui/JMainFrame.java
@@ -0,0 +1,14 @@
+package co.edu.unal.isi.taxiagent2.gui;
+
+/**
+ * The main frame: this is the entry point of the program.
+ * @author Mauricio Mejía
+ *
+ */
+public class JMainFrame {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/co/edu/unal/isi/taxiagent2/model/Position.java
@@ -0,0 +1,61 @@
+package co.edu.unal.isi.taxiagent2.model;
+
+public class Position {
+	private int i, j;
+	
+	public Position(int x, int y) {
+		i = x; j = y;
+	}
+	
+	public Position() {
+		i = 0; j = 0;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + i + ", " + j + ")";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + i;
+		result = prime * result + j;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (i != other.i)
+			return false;
+		if (j != other.j)
+			return false;
+		return true;
+	}
+	
+	
+}
